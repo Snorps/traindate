21bbaa9cfbcd13530354788f354e90a55fb17114
Virtualistik <virtualistik@hotmail.co.uk>
2017-10-06 14:50:10 +0100

    uihandler patch

--- Assets/UIhandler.cs
+++ Assets/UIhandler.cs
@@ -6,40 +6,45 @@ using UnityEngine.UI;
 public class UIhandler : MonoBehaviour {
 
     Text dialogText;
+    public bool dialogStaggering;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		dialogText = this.gameObject.transform.Find("DialogText").gameObject.GetComponent<Text>();
         //changeImageSprite("Character", "traumatrain");
+        dialogStaggering = false;
+
     }
 
     public void changeText(string str, float textDelay = 0.07f) //to be called by the event manager
     {
-        clearText();
-        if (staggeringState == StaggeringState.staggering)
+        if (dialogStaggering)
         {
-            staggeringState = StaggeringState.skip;
-        } else
-        {
-            staggeringState = StaggeringState.skipped;
+            dialogStaggering = false;
         }
-        StartCoroutine(StaggerText(str, textDelay));
+        else
+        {
+            clearText();
+            dialogStaggering = true;
+            StartCoroutine(StaggerText(str, textDelay));
+        }
+        
     }
 
-    public enum StaggeringState { staggering, skipped, skip };
-    public StaggeringState staggeringState;
+
     IEnumerator StaggerText(string str, float textDelay) //don't know why this is how you wait in unity
     {
         for (int i = 0; i < str.Length; i++) //for every letter in the string to be displayed
         {
-            if (!(staggeringState == StaggeringState.skip))
+            Debug.Log(dialogStaggering);
+            if (dialogStaggering && (i+1) < str.Length)
             {
                 dialogText.text = dialogText.text.Insert(dialogText.text.Length, str.Substring(i, 1)); //append curent char text in dialog box
                 yield return new WaitForSeconds(textDelay);
             } else
             {
-                dialogText.text = dialogText.text.Insert(dialogText.text.Length, str.Substring(i, str.Length - i)); //append rest of string to text in dialog box
-                staggeringState = StaggeringState.skipped;
+                dialogText.text = dialogText.text.Insert(dialogText.text.Length, str.Substring(i, str.Length-i)); //append rest of text in dialog box
+                dialogStaggering = false;
                 break; //cancel for if told to skip staggering
             }
         }
